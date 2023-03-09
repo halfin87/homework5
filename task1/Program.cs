e7f8bc358bd07225ce73d8e67684869820fd1781
halfin rodion <halfin87@mail.ru>
2023-03-10 00:13:18 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] Randomarray()
+
+{
+  int[] array = new int[5];
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = Random.Shared.Next(100, 1000);
+  }
+  return array;
+}
+
+int Sumelements(int[] array)
+{
+  int sum = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (i / 2 == 0)
+    {
+      sum ++;
+    }
+  }
+  return sum;
+}
+
+int[] myArray = Randomarray();
+int positivedigit = Sumelements(myArray);
+Console.WriteLine($"получившийся массив = [{string.Join(", ", myArray)}]");
+Console.WriteLine($"сумма четных чисел = {positivedigit}");
